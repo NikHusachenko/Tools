b3b1a26a5745ceccf683010111e43b85ca91e7ae
NikHusachenko <nikgusachenko@gmail.com>
2023-04-08 17:26:04 +0300

    Добавьте файлы проекта.

---
+++ Tools.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Tools.Desktop
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
