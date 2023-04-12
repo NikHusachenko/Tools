69a846a269190ea4e826c7212d1fa644f4816e50
Oleg <olegredko45@gmail.com>
2023-04-12 21:08:37 +0300

    Update GraphicEquipmentPage design

---
+++ Tools.Desktop/EquipmentForms/Pages/GraphicEquipmentPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Tools.Desktop.EquipmentForms.Pages
+{
+	/// <summary>
+	/// Логика взаимодействия для GraphicEquipmentPage.xaml
+	/// </summary>
+	public partial class GraphicEquipmentPage : Page
+	{
+		public GraphicEquipmentPage()
+		{
+			InitializeComponent();
+		}
+
+		private void FirstRadioBtn_Click(object sender, RoutedEventArgs e)
+		{
+			pagesFrame.Navigate(new SortByDepartment());
+		}
+	}
+}
--- Tools.Desktop/MainWindow.xaml.cs
+++ Tools.Desktop/MainWindow.xaml.cs
@@ -16,21 +16,16 @@ namespace Tools.Desktop
 		private void EquipmentMenuItem_Click(object sender, RoutedEventArgs e)
 		{
             pagesFrame.Navigate(new EquipmentPage());
-          //  Equipment equipment = new Equipment();
-          //  equipment.ShowDialog();
         }
 
 		private void AddEquipmentMenuItem_Click(object sender, RoutedEventArgs e)
 		{
             pagesFrame.Navigate(new AddEquipmentPage());
-            //AddEquipment addEquipment= new AddEquipment();
-           //addEquipment.ShowDialog();
 		}
 
 		private void GraphicMenuItem_Click(object sender, RoutedEventArgs e)
 		{
-          //  GraphicOfEquipmentSurveys graphic = new GraphicOfEquipmentSurveys();
-          //  graphic.ShowDialog();
+            pagesFrame.Navigate(new GraphicEquipmentPage());
 		}
 	}
 }
