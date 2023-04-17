702bde7a95070a11ddd36b4ca3bdb1526d33b77f
Oleg <olegredko45@gmail.com>
2023-04-17 19:42:07 +0300

    Add GlobalErrorLabel, Replace SortBy...Page on ComboBox, Remove calendar buttons and CatalogEquipmentPage

--- Tools.Desktop/Pages/EditEquipmentPage.xaml.cs
+++ Tools.Desktop/Pages/EditEquipmentPage.xaml.cs
@@ -12,40 +12,5 @@ namespace Tools.Desktop.Pages
 		{
 			InitializeComponent();
 		}
-		private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
-		{
-			addEquipmentGrid.Visibility = Visibility.Collapsed;
-			catalogEquipmentGrid.Visibility = Visibility.Visible;
-			equipmentFrame.Navigate(new CatalogEquipmentPage(addEquipmentGrid));
-		}
-
-		//private void CalendarButton_Click(object sender, System.Windows.RoutedEventArgs e)
-		//{
-		//	datePicker.Visibility = Visibility.Visible;
-		//}
-		private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
-		{
-			// handle the selected date change event here
-		}
-
-		private void dateCreationPicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
-		{
-
-		}
-
-		private void CalendarCreationButton_Click(object sender, RoutedEventArgs e)
-		{
-			dateCreationPicker.Visibility = Visibility.Visible;
-		}
-
-		private void dateImplementetionPicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
-		{
-
-		}
-
-		private void CalendarImplementetionButton_Click(object sender, RoutedEventArgs e)
-		{
-			dateImplementetionPicker.Visibility = Visibility.Visible;
-		}
 	}
 }
