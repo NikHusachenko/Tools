78d737bee8c652e8d966de9f79236742da8c050a
NikHusachenko <nikgusachenko@gmail.com>
2023-05-10 10:23:08 +0300

    Append window for tools editing and deleting

--- Tools.Desktop/Windows/ToolDataViewWindow.xaml.cs
+++ Tools.Desktop/Windows/ToolDataViewWindow.xaml.cs
@@ -6,23 +6,30 @@ namespace Tools.Desktop.Windows
 {
     public partial class ToolDataViewWindow : Window
     {
+
         public ToolDataViewWindow(ToolEntity toolEntity)
         {
             InitializeComponent();
+        }
 
-            organizationTextBox.Text = toolEntity.OrganizationUnit.Name;
-            equipmentGroupTextBox.Text = toolEntity.Subgroup.Group.Name;
-            equipmentSubgroupTextBox.Text = toolEntity.Subgroup.Name;
-            equipmentNameTextBox.Text = toolEntity.Name;
-            equipmentBrandTextBox.Text = toolEntity.Brand;
-            equipmentRegistrationTextBox.Text = RegistrationTypeDisplay.GetDisplayName(toolEntity.Registration);
-            equipmentRegistrationNumberTextBox.Text = toolEntity.RegistrationNumber;
-            equipmentIntraFactoryNumberTextBox.Text = toolEntity.IntraFactoryNumber;
-            equipmentManufacturerTextBox.Text = toolEntity.Manufacturer;
-            equipmentFactoryNumberTextBox.Text = toolEntity.FactoryNumber;
-            dateOfCreatingTextBox.Text = toolEntity.CreatingDate.ToString("dd.MM.yyyy");
-            commissioningDateTextBox.Text = toolEntity.CommissioningDate.ToString("dd.MM.yyyy");
-            equipmentExpirationYearTextBox.Text = toolEntity.ExpirationYear.ToString();
+        private void examinationTypeComboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void examinationReasonComboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void examinationNatureComboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
